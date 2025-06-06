85693bd10d4dae5bf2c2f76b43ea9099246773b9
Paul Wu <paul_wu@gemteks.com>
2025-06-06 15:02:09 +0800

    Fixed vite.config.ts error

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,4 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    // Add asset fingerprinting with hash
-    rollupOptions: {
-      output: {
-        // Use content hash for assets
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'js/[name]-[hash].js',
-        entryFileNames: 'js/[name]-[hash].js',
-      }
-    }
-  }
 })
